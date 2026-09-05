10a714070f1902bef82935d0f64d6b2bf7aa93a2
snikskytt3r <snikskytter@gmail.com>
2026-09-05 09:05:02 -0600

    Implementación de nueva estructura de metadata para enriquecimiento y claridad en la información de extracción

--- src/ingestion.py
+++ src/ingestion.py
@@ -16,25 +16,48 @@ class Balldontlie():
 
 
     def get_games_balldontlie(self):
-
         response = requests.get(url=self.url_balldontlie, headers=self.headers)
-        print("Request Status code:", response.status_code)
+        http_status = response.status_code
+        print("Request Status code:", http_status)
         response_content = response.json()
-        return response_content
+        return response_content, http_status
 
 
 def main():
 
     client_balldontlie = Balldontlie()
-    games_balldontlie = client_balldontlie.get_games_balldontlie()
+    games_balldontlie, status_code_response = client_balldontlie.get_games_balldontlie()
 
     data_games = games_balldontlie['data']
-    meta_games = games_balldontlie['meta']
-
     print("Cantidad de juegos detectados:", len(data_games))
 
-    utc_timestamp = datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S")
-    
+    if data_games:
+        games_dates = [game['date'] for game in data_games if game.get('date')]
+        min_game_date = min(games_dates) if games_dates else None
+        max_game_date = max(games_dates) if games_dates else None
+
+    extraction_datetime = datetime.now(timezone.utc)
+
+    extracted_at_utc = extraction_datetime.isoformat()
+    utc_timestamp = extraction_datetime.strftime("%Y%m%d_%H%M%S")
+
+    ingestion_metadata = {
+        "source": "balldontlie",
+        "league": "nba",
+        "entity": "games", 
+        "endpoint": client_balldontlie.url_balldontlie,
+        "extracted_at_utc": extracted_at_utc,
+        "http_status_code": status_code_response,
+        "records_received": len(data_games),
+        "min_game_date": min_game_date,
+        "max_game_date": max_game_date
+    }
+
+    raw_document = {
+        "ingestion_metadata": ingestion_metadata,
+        "source_response": games_balldontlie
+    }
+
     root_dir = Path(__file__).resolve().parent.parent
     output_dir = root_dir/"data"/"raw"/"balldontlie"/"nba"/"games"
 
@@ -44,7 +67,7 @@ def main():
     file_path = output_dir / file_name
 
     with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(games_balldontlie, f, ensure_ascii=False, indent=4)
+        json.dump(raw_document, f, ensure_ascii=False, indent=4)
 
     print(f"\n Archivo guardado exitosamente en: {file_path}")
 
